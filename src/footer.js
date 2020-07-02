3101d399cd1a625a7092e53a47fa31f00fea2099
Sandra Li <sandra@Sandras-MacBook-Air.local>
2020-07-02 12:04:32 -0700

    first commit

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+function Footer(){
+  const [text,setText] = useState('')
+  return <footer class="footer">
+    <input class="text-input" value={text} 
+      onChange={e=> setText(e.target.value)}
+    />
+    <button class="send-button">
+      ↑
+    </button>
+  </footer>
+}
+export default Footer
