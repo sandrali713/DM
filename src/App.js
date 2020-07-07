f904a79e4889effb0fb22f1e738dd311d1236fc7
Sandra Li <sandra@Sandras-MacBook-Air.local>
2020-07-07 11:42:17 -0700

    send messages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Footer from './footer'
+import Message from './message'
+
 
 function App() {
+  const [messages,setMessages] = useState([])
   return (
+    <main className="main">
     <div className="App">
     <header>
         <span class = "title">Chatter</span>
     </header>
-    <Footer/>
+    {messages.map((m,i)=> <Message key={i} text={m} />)}
+
+    <Footer
+        onSend={(text)=> setMessages([...messages, text])} 
+    />
+      
     </div>
   );
 }
